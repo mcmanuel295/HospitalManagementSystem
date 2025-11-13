82f48d14009551793350d3a488c3860242038585
unknown <mcmanuel755@gmail.com>
2025-11-13 06:51:18 +0100

    finishing touches

--- src/main/java/com/mcmanuel/HospitalManagementSystem/service/intf/UserService.java
+++ src/main/java/com/mcmanuel/HospitalManagementSystem/service/intf/UserService.java
@@ -3,7 +3,7 @@ package com.mcmanuel.HospitalManagementSystem.service.intf;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-public interface UserService <T>{cls
+public interface UserService <T>{
     T getUserById(String userId) throws NoSuchElementException;
     T getUserByEmail(String email) throws NoSuchElementException;
     List<T> getAllUser();
