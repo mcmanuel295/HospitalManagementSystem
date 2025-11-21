fec83e27bc283a3335dc8f1e2e7b9c3914e7ce99
unknown <mcmanuel755@gmail.com>
2025-11-21 13:38:09 +0100

    removed unused imports

--- src/main/java/com/mcmanuel/HospitalManagementSystem/HospitalManagementSystemApplication.java
+++ src/main/java/com/mcmanuel/HospitalManagementSystem/HospitalManagementSystemApplication.java
@@ -1,17 +1,12 @@
 package com.mcmanuel.HospitalManagementSystem;
 
-import com.mcmanuel.HospitalManagementSystem.entity.Owner;
-import com.mcmanuel.HospitalManagementSystem.service.impl.AdminServiceImpl;
 import com.mcmanuel.HospitalManagementSystem.service.impl.OwnerServiceImpl;
 import com.mcmanuel.HospitalManagementSystem.service.intf.AdminService;
-import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityExistsException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -27,6 +22,7 @@ public class HospitalManagementSystemApplication {
 		return args -> {
 			try {
 				adminService.addAdminByEmail(ownerService.addOwner().getEmail());
+				log.info("admin added");
 			}
 			catch (EntityExistsException ex){
 				log.error("Entity already exist{}",ex.getMessage());
