ae1b6be08949370eba6b961a880077105418da11
unknown <mcmanuel755@gmail.com>
2025-11-21 13:24:13 +0100

    moved the addOwner operation from Post-construct to commandLineRunner

--- src/main/java/com/mcmanuel/HospitalManagementSystem/HospitalManagementSystemApplication.java
+++ src/main/java/com/mcmanuel/HospitalManagementSystem/HospitalManagementSystemApplication.java
@@ -1,15 +1,39 @@
 package com.mcmanuel.HospitalManagementSystem;
 
+import com.mcmanuel.HospitalManagementSystem.entity.Owner;
+import com.mcmanuel.HospitalManagementSystem.service.impl.AdminServiceImpl;
+import com.mcmanuel.HospitalManagementSystem.service.impl.OwnerServiceImpl;
+import com.mcmanuel.HospitalManagementSystem.service.intf.AdminService;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class HospitalManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementSystemApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner addOwner(AdminService adminService,OwnerServiceImpl ownerService){
+		return args -> {
+			try {
+				adminService.addAdminByEmail(ownerService.addOwner().getEmail());
+			}
+			catch (EntityExistsException ex){
+				log.error("Entity already exist{}",ex.getMessage());
+			}
+			catch (Exception ex){
+				log.error("error occurred: {}",ex.getMessage());
+			}
+		};
+	}
 }
--- src/main/java/com/mcmanuel/HospitalManagementSystem/service/impl/OwnerServiceImpl.java
+++ src/main/java/com/mcmanuel/HospitalManagementSystem/service/impl/OwnerServiceImpl.java
@@ -5,45 +5,26 @@ import com.mcmanuel.HospitalManagementSystem.entity.User;
 import com.mcmanuel.HospitalManagementSystem.pojo.Role;
 import com.mcmanuel.HospitalManagementSystem.repository.OwnerRepository;
 import com.mcmanuel.HospitalManagementSystem.service.intf.AdminService;
-import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityExistsException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class OwnerServiceImpl {
-    private static final Logger log = LoggerFactory.getLogger(OwnerServiceImpl.class);
     private final AdminService adminService;
     private final OwnerRepository ownerRepo;
     private final PasswordEncoder passwordEncoder;
 
 
-    private OwnerServiceImpl(AdminService adminService, OwnerRepository ownerRepo, PasswordEncoder passwordEncoder) {
-        this.adminService = adminService;
-        this.ownerRepo = ownerRepo;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @PostConstruct
-    private void automaticAdding(){
-        try {
-            adminService.addAdminByEmail(addOwner().getEmail());
-        }
-        catch (EntityExistsException ex){
-            log.error("Entity already exist{}",ex.getMessage());
-        }
-        catch (Exception ex){
-            log.error("error occurred: {}",ex.getMessage());
-        }
-    }
-
-    private Owner addOwner(){
+    public Owner addOwner() throws EntityExistsException{
         Owner owner =createOwner();
-        System.out.println("owner :"+owner);
+        log.info("owner: {}",owner);
         return ownerRepo.save(owner);
     }
 
